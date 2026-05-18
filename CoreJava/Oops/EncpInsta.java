68b2502d12292c8f463d10606253059235bc37fc
Manikanchara <kancharanamanikanta26@email.com>
2026-05-18 14:32:12 +0530

    Encapsulation

---
+++ CoreJava/Oops/EncpInsta.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Insta{
+static String password ="Mani123";     //Data Hiding
+void setPass(String password){         //used to Update or Reset Private fields
+this.password=password;
+}
+String getPass(){                       // used to Display Private fields
+return password;
+}
+}
+class EncpInsta{
+Scanner sc = new Scanner(System.in);
+void m1(){
+Insta obj = new Insta();                      // Has-A relationship
+System.out.println(obj.getPass());
+obj.setPass(sc.next());
+System.out.println(obj.getPass());
+}
+public static void main(String[] args){
+  EncpInsta x = new EncpInsta();
+  x.m1();
+}
+}
---
+++ CoreJava/Oops/User_A_Relation.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class A{
+static Scanner sc = new Scanner(System.in);
+int n=sc.nextInt();
+String m1(int n){
+System.out.println("Local variable "+n);
+System.out.println("Local variable "+this.n);
+return sc.next();
+}
+A(){
+this(sc.nextInt());
+System.out.println("Parent class Constructor");
+}
+A(int n){
+System.out.println("Base class Constructor with parameter is " +n);
+}
+}
+
+class User_A_Relation{
+static Scanner sc = new Scanner(System.in);
+User_A_Relation(){
+this(sc.nextInt());
+System.out.println("Child class Constructor ");
+}
+
+User_A_Relation(int a){
+System.out.println("derived class Constructor with parameter is " +a);
+}
+
+void m2(){
+A obj = new A();
+System.out.println(obj.m1(sc.nextInt()));
+}
+
+public static void main(String[] args){
+User_A_Relation x = new User_A_Relation();
+x.m2();
+}
+}
