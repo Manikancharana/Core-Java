e0772a621ad47e08fca944feefe9a5a473118ca8
Manikanchara <kancharanamanikanta26@gmail.com>
2026-06-14 17:09:44 +0530

    Alphabet pattern

--- Programming/Pattern/AlphabetPattern.java
+++ Programming/Pattern/AlphabetPattern.java
@@ -4,7 +4,8 @@ class AlphabetPattern{
 	static void a(){
 	    for(int i=1;i<=7;i++){
       	    for(int j=7;j>=i;j--){
-            System.out.print(" ");
+          
+                System.out.print(" ");
       	  }
       	  for(int j=1;j<=i;j++){
             if(j==1 || j==i || i==4){
